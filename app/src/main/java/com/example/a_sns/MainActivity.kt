692a72de3b0ab51e4c35f16eae4ec2a0b64d5d8f
shinmink <sinmk99@hansung.ac.kr>
2022-11-01 20:44:44 +0900

    테스트 주석 추가

--- app/src/main/java/com/example/a_sns/MainActivity.kt
+++ app/src/main/java/com/example/a_sns/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 아 학교 떄려치고 싶다 시발..
     }
 }
