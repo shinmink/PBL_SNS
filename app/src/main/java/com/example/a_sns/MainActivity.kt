f65a77cc439c51c7beb53d353d5071761e9a2f84
pc <ddhmh21@naver.com>
2022-11-06 05:20:03 +0900

    로그인 후 메인화면, 마이페이지 화면

--- app/src/main/java/com/example/a_sns/MainActivity.kt
+++ app/src/main/java/com/example/a_sns/MainActivity.kt
@@ -1,14 +1,48 @@
 package com.example.a_sns
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 아 학교 정말 즐겁다 !
-        // 모두의 오머대학교 !
+        Firebase.auth.signInWithEmailAndPassword("a@a.com", "123456")
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    println("########## Login Success")
+                    println(Firebase.auth.currentUser?.uid)
+                } else {
+                    it.exception?.message
+                    println("########## Login Failed ${it.exception?.message}")
+                }
+            }
+
+        bottom_navigation.selectedItemId = R.id.action_home
+
+        bottom_navigation.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.action_home ->{
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.action_account ->{
+                    var userFragment = UserFragment()
+                    var bundle = Bundle()
+                    var uid = FirebaseAuth.getInstance().currentUser?.uid
+
+                    bundle.putString("destinationUid",uid)
+                    userFragment.arguments = bundle
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/a_sns/UserFragment.kt
@@ -0,0 +1,40 @@
+package com.example.a_sns
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_user.view.*
+
+class UserFragment : Fragment(){
+    var fragmentView : View? = null
+    var firestore : FirebaseFirestore? = null
+    var uid : String? = null
+    var auth : FirebaseAuth? = null
+    var currentUserUid : String? = null
+    companion object {
+        var PICK_PROFILE_FROM_ALBUM = 10
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
+        uid = arguments?.getString("destinationUid")
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        currentUserUid = auth?.currentUser?.uid
+
+        if(uid == currentUserUid){
+            //MyPage
+            fragmentView?.account_btn_follow_signout?.text = getString(R.string.signout)
+            fragmentView?.account_btn_follow_signout?.setOnClickListener {
+                activity?.finish()
+                startActivity(Intent(activity,LoginActivity::class.java))
+                auth?.signOut()
+            }
+        }
+        return fragmentView
+    }
+}
