8a6562461a9566cfd76a97842801059ecaf7e0de
shinmink <sinmk99@hansung.ac.kr>
2022-11-08 14:55:58 +0900

    중간 초기 자료

--- app/src/main/java/com/example/a_sns/LoginActivity.kt
+++ app/src/main/java/com/example/a_sns/LoginActivity.kt
@@ -6,56 +6,98 @@ import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         auth = FirebaseAuth.getInstance()
+
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+
+        Firebase.auth.signInWithEmailAndPassword("a@a.com", "123456")
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    println("########## Login Success")
+                    println(Firebase.auth.currentUser?.uid)
+                } else {
+                    it.exception?.message
+                    println("########## Login Failed ${it.exception?.message}")
+                }
+            }
+
+        bottom_navigation.selectedItemId = R.id.action_home
+
+        bottom_navigation.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.action_home ->{
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.action_account ->{
+                    var userFragment = UserFragment()
+                    var bundle = Bundle()
+                    var uid = FirebaseAuth.getInstance().currentUser?.uid
+
+                    bundle.putString("destinationUid",uid)
+                    userFragment.arguments = bundle
+                    supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                }
+            }
+        }
+
+
     }
 
     override fun onStart() {
         super.onStart()
-        moveMainPage(auth?.currentUser)
+        moveMainPage(auth.currentUser)
     }
 
     fun signinAndSignup(){
-        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
-            ?.addOnCompleteListener {
-                    task ->
+        auth.createUserWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            .addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     //Creating a user account
+                    // 계정 만들기, 회원가입하기
                     moveMainPage(task.result?.user)
                 }else if(task.exception?.message.isNullOrEmpty()){
                     //Show the error message
+                    // 로그인 실패로 인한 에러 메세지 출력
                     Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
                 }else{
                     //Login if you have account
+                    // 계정이 이미 있다면 !
                     signinEmail()
                 }
             }
     }
 
     fun signinEmail(){
-        auth?.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
-            ?.addOnCompleteListener {
-                    task ->
+        auth.signInWithEmailAndPassword(email_edittext.text.toString(),password_edittext.text.toString())
+            .addOnCompleteListener { task ->
                 if(task.isSuccessful){
                     //Login
+                    // 로그인 성공
                     moveMainPage(task.result?.user)
                 }else{
                     //Show the error message
+                    // 로그인 실패로 인한 에러 메세지 출력
                     Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
                 }
             }
     }
 
     fun moveMainPage(user:FirebaseUser?){
+        // 로그인이 성공적일 경우 메인화면으로 넘어가기
         if(user != null){
             startActivity(Intent(this,MainActivity::class.java))
             finish()
--- app/src/main/java/com/example/a_sns/MainActivity.kt
+++ app/src/main/java/com/example/a_sns/MainActivity.kt
@@ -1,26 +1,26 @@
 package com.example.a_sns
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import android.Manifest
+import androidx.viewbinding.ViewBinding
 import android.content.pm.PackageManager
-import android.graphics.Color
-import android.os.Build
-import androidx.fragment.app.Fragment
-import com.example.a_sns.ui.LoginFragment
-import com.example.a_sns.ui.RequestPermissionFragment
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.fragment.app.Fragment
 import com.example.a_sns.databinding.ActivityMainBinding
+import com.example.a_sns.ui.RequestPermissionFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +30,6 @@ class MainActivity : AppCompatActivity() {
 
     //var user : String = "User"
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -38,88 +37,17 @@ class MainActivity : AppCompatActivity() {
 
         initSetting()
 
-        // 알림 허용 설정
-        requestSinglePermission(Manifest.permission.ACCESS_NOTIFICATION_POLICY)
-
         // 사용자 셋팅
         auth = Firebase.auth
 
-        /* searchview dropdown
-        val searchAutoComplete: SearchView.SearchAutoComplete =
-            binding.searchView.findViewById(androidx.appcompat.R.id.search_src_text)
-
-        searchAutoComplete.setTextColor(Color.BLACK)
-        searchAutoComplete.setDropDownBackgroundResource(android.R.color.white)
-
-        //가라로 데이터 초기화
-         val datas = arrayOf(
-             "피카츄",
-             "꼬부기",
-             "파이리"
-         )
-
-        // 참고 예시 아래
-        //val SeoulBusdatas = RouteTools.getRouteNames().toList()
-
-        searchAutoComplete.setAdapter(
-            ArrayAdapter(
-                this,
-                android.R.layout.simple_list_item_1,
-                datas // 가라 데이터
-            )
-       )
-         */
-    }
-
-    // 알림 허가 받는 함수
-    private fun requestSinglePermission(permission: String) {
-        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED)
-            return
-
-        val requestPermLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            if (it == false) { // permission is not granted!
-                AlertDialog.Builder(this).apply {
-                    setTitle("Warning")
-                    setMessage(getString(R.string.no_permission, permission))
-                }.show()
-            }
-        }
-
-        if (shouldShowRequestPermissionRationale(permission)) {
-            // you should explain the reason why this app needs the permission.
-            AlertDialog.Builder(this).apply {
-                setTitle("Reason")
-                setMessage(getString(R.string.req_permission_reason, permission))
-                setPositiveButton("Allow") { _, _ -> requestPermLauncher.launch(permission) }
-                setNegativeButton("Deny") { _, _ -> }
-            }.show()
-        } else {
-            // should be called in onCreate()
-            requestPermLauncher.launch(permission)
-        }
-    }
-
-
-    //TODO : 화면 초기 셋팅값 설정 함수
-    private fun checkLocationPermission(): Boolean {
-        val fineLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-        val coarseLocationPermission = ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-        return fineLocationPermission && coarseLocationPermission
-    }
-
         // start LoginFragment
         //changeFragment(LoginFragment())
-
         // start RequestPermissionFragment
+
         changeFragment(RequestPermissionFragment())
-    }
+
+        // 임시 로그인 액티비티
+        //startActivity(Intent(this,LoginActivity::class.java))
 
         Firebase.auth.signInWithEmailAndPassword("a@a.com", "123456")
             .addOnCompleteListener {
@@ -152,18 +80,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+    }
+
     // change Fragment
     fun changeFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .setCustomAnimations(androidx.appcompat.R.anim.abc_fade_in, androidx.appcompat.R.anim.abc_fade_out)
             .setReorderingAllowed(true)
-            .replace(R.id.frameLayout, fragment)
+            .replace(R.id.main_content, fragment)
             .addToBackStack(null)
             .commit()
-            
-    }
-}
 
+    }
+
+
+    //TODO : 화면 초기 셋팅값 설정 함수
+    private fun checkLocationPermission(): Boolean {
+        val fineLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val coarseLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        return fineLocationPermission && coarseLocationPermission
+    }
 
 
     //TODO : 화면 초기 셋팅값 설정 함수
@@ -173,22 +117,17 @@ class MainActivity : AppCompatActivity() {
         // 2. 툴바 사용 설정
         setSupportActionBar(binding.MainToolbar)
         // 3. 툴바 왼쪽 버튼 설정
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
-        supportActionBar!!.setHomeAsUpIndicator(R.drawable.baseline_home_24)  // 왼쪽 버튼 아이콘 설정
-        supportActionBar!!.setTitle("HANSUNG SNS")
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
+        //supportActionBar!!.setHomeAsUpIndicator(R.drawable.baseline_home_24)  // 왼쪽 버튼 아이콘 설정
+        //supportActionBar!!.setTitle("HANSUNG SNS")
 
     }
 
 
-    // 4. 툴바 메뉴 좌측 상단 버튼 클릭 됐을 때 클릭 // @@
+    // 4. 툴바 메뉴 좌측 상단 버튼 클릭 됐을 때 클릭
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item!!.itemId) {
-            android.R.id.home -> { // 메인 ( 즉, 유저 메뉴에서 좌측 상단 버튼은 시작 페이지로가는 버튼 )
-                Toast.makeText(this,"Main",Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this@MainActivity,MainActivity::class.java))
-            }
-
-            R.id.Posting_icon -> { // 알림 페이지로 이동
+            R.id.Back_icon -> { // 뒤로
                 Toast.makeText(this, "Post My Story", Toast.LENGTH_SHORT).show()
                 startActivity(Intent(this,PostingActivity::class.java))
             }
@@ -205,35 +144,6 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    // + 검색 실행 시 검색 전용 페이지로 넘어가기
-    // + 검색 전용 페이지에서 ( 자동완성이나 검색기록 구현 )
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.toolbar,menu)
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
-            android.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String): Boolean {
-                //TODO("검색어 변경 이벤트, 입력 할때마다 반응")
-                return true
-            }
-
-            override fun onQueryTextSubmit(query: String): Boolean {
-                //TODO("키보드의 검색 버튼을 클릭한 순간의 이벤트,  즉 입력 완료")
-                Toast.makeText(this@MainActivity, "검색 결과 : $query", Toast.LENGTH_SHORT).show()
-                // 검색 전용 페이지로 넘어가면서 검색 결과 보내기
-                startActivity(Intent(this@MainActivity, SearchingActivity::class.java).also {
-                    it.putExtra("queryanswer", query)
-                })
-
-                return true
-            }
-
-        })
-        return super.onCreateOptionsMenu(menu)
-    }
-
-
-
 
 
 
