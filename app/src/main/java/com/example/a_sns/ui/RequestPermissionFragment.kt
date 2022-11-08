4fca2c7bcc9bc67a9bdc0b1a29755db6f7583703
JeongKiSeong <rltjd8714@gmail.com>
2022-11-08 23:14:42 +0900

    api 33(티라미수) 이상 -> POST_NOTIFICATIONS                 이하 -> READ_EXTERNAL_STORAGE

--- app/src/main/java/com/example/a_sns/ui/RequestPermissionFragment.kt
+++ app/src/main/java/com/example/a_sns/ui/RequestPermissionFragment.kt
@@ -1,10 +1,13 @@
 package com.example.a_sns.ui
 
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.*
+import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
@@ -12,55 +15,67 @@ import com.example.a_sns.StartActivity
 import com.example.a_sns.R
 
 class RequestPermissionFragment : Fragment(R.layout.request_permission_fragment) {
-    // initialization
-    private val requestPermLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { // 권한 요청 컨트랙트
-        if (it == false) { // permission is not granted!
-            AlertDialog.Builder(requireContext()).apply {
+    private var perms = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) // Android 10.0 이상
+        arrayOf(POST_NOTIFICATIONS)
+    else
+        arrayOf(READ_EXTERNAL_STORAGE)
+
+    // initialization of the request permission launcher
+    private val requestPermLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        val noPerms = it.filter { item -> !item.value }.keys
+        if (noPerms.isNotEmpty()) { // there is a permission which is not granted!
+            AlertDialog.Builder(requireActivity()).apply {
                 setTitle("경고")
-                setMessage("앱을 사용하려면 권한을 허용해야 합니다.")
-                setPositiveButton("확인") { _, _ ->
-                    // 권한 요청 다시하기
-                    requestSinglePermission(READ_EXTERNAL_STORAGE)
-                }
+                //setMessage("앱을 사용하려면 권한을 허용해야 합니다.")
+                setMessage(getString(R.string.no_permission, noPerms))
+                setPositiveButton("확인") { _, _ -> }
             }.show()
+        }
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // if permission is granted, start LoginFragment
+        val requestPerms = perms.filter { checkSelfPermission(requireActivity(), it) != PackageManager.PERMISSION_GRANTED } // 여러 권한 확인
+        if (requestPerms.isEmpty()) {
+            (requireActivity() as StartActivity).changeFragment(LoginFragment())
+            requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // if permission is granted, start LoginFragment
-        if (checkSelfPermission(requireContext(), READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            (requireActivity() as StartActivity).changeFragment(LoginFragment())
-            //activity?.supportFragmentManager?.beginTransaction()?.remove(this)?.commit()
-        }
+
 
         val allowBtn = view.findViewById<View>(R.id.requestPermission_allowBtn)
         allowBtn.setOnClickListener {
-            requestSinglePermission(READ_EXTERNAL_STORAGE)
+            requestMultiplePermission(perms)
+
+            // if permission is granted, start LoginFragment
+            val requestPerms = perms.filter { checkSelfPermission(requireActivity(), it) != PackageManager.PERMISSION_GRANTED } // 여러 권한 확인
+            if (requestPerms.isEmpty())
+                (requireActivity() as StartActivity).changeFragment(LoginFragment())
         }
     }
 
-    private fun requestSinglePermission(permission: String) { // 한번에 하나의 권한만 요청하는 예제
-        if (checkSelfPermission(requireActivity(), permission) == PackageManager.PERMISSION_GRANTED) // 권한 유무 확인
+    private fun requestMultiplePermission(perms: Array<String>) {
+        val requestPerms = perms.filter { checkSelfPermission(requireActivity(), it) != PackageManager.PERMISSION_GRANTED } // 여러 권한 확인
+        if (requestPerms.isEmpty())
             return
 
-        // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-
-        if (shouldShowRequestPermissionRationale(permission)) { // 권한 설명 필수 여부 확인
+        val showRationalePerms = requestPerms.filter { shouldShowRequestPermissionRationale(it) } // 권한 요청을 거절한 적이 있는지 확인
+        if (showRationalePerms.isNotEmpty()) {
             // you should explain the reason why this app needs the permission.
-            AlertDialog.Builder(requireContext()).apply {
-                setTitle("권한 요청")
-                setMessage("이 앱은 사진을 불러오기 위해 권한을 요청합니다.")
-                setPositiveButton("허용") { _, _ -> requestPermLauncher.launch(permission) }
-                setNegativeButton("거부") { _, _ ->
-                    (requireActivity() as StartActivity).finish()
-                }
+            AlertDialog.Builder(requireActivity()).apply {
+                setTitle("경고")
+                setMessage("허용해야하는 이유")
+                setPositiveButton("확인") { _, _ -> requestPermLauncher.launch(perms) }
             }.show()
         } else {
-            // should be called in onCreate()
-            requestPermLauncher.launch(permission) // 권한 요청 시작
+            requestPermLauncher.launch(requestPerms.toTypedArray())
         }
     }
 }
